c44bf4c9b0049cbd4fc45db597bbc4ed25e7f6ca
Basit Ali <alib4111@gmail.com>
2023-09-23 03:07:00 +0500

    first lesson

---
+++ variables/main.js
@@ -0,0 +1,53 @@
+// Variables;
+// Syntax
+// keywords in javascript
+
+// let myFirstName; // declared a variable
+
+// myFirstName = 'Basit'; // defined a variable
+
+// console.log(myFirstName)
+
+// let myLastName = 'Ali'; // declared & defined
+// console.log(myLastName);
+// myLastName = 'Sohail';
+// console.log(myLastName);
+
+const myLastName = 'Ali'; // declared & defined  == read-only
+console.log(myLastName);
+// myLastName = 'Sohail';
+// console.log(myLastName);
+
+let myPhone = "Ali's phone";
+
+function anyFunction() {
+	let funVariable = 'some value'; // local variable
+	console.log(funVariable);
+}
+
+//console.log(funVariable);
+
+// console.log(myPhone);
+
+// Data Types
+// 'Ali'   =>String
+
+let myAge = 26; // Number '26'+5; =265
+
+console.log(myAge);
+
+let male = true; // Boolean
+
+let haveKids = null; // Null
+
+// Object  =>Arrays, Object, Classes....
+// copyright@2024
+
+let listOfYears = [2000, 2001, 2002, 2003];
+// let person  ={}
+
+console.log(typeof haveKids);
+
+// Scope of Variables
+// Global Scope varials  => Global Variables
+// Local Scope variabls  => Local Variables
